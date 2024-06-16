26bcd9de549732d047071326c0df98b9328fa431
Leslie Mwangi <luciannahphoi@gmail.com>
2024-06-16 20:13:43 +0300

    card css

--- frontend/src/components/PropertyCard.jsx
+++ frontend/src/components/PropertyCard.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+
 const PropertyCard = ({ property }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={property.image_url} alt={property.name} />
-      <Card.Body>
+    <Card className="custom-card">
+      <Card.Img
+        variant="top"
+        src={property.image_url}
+        alt={property.name}
+        className="custom-card-img"
+      />
+      <Card.Body className="custom-card-body">
         <Card.Title>{property.name}</Card.Title>
         <Card.Text>{property.description.substring(0, 100)}...</Card.Text>
         <Link to={`/properties/${property.id}`}>
